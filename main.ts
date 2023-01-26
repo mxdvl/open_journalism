99250196bdbfaf7f7100aff9415b71ab7f146e77
Max Duval <max.duval@theguardian.com>
2023-01-26 14:33:08 +0000

    fix: working index page

--- main.ts
+++ main.ts
@@ -2,24 +2,47 @@ import { serve } from "https://deno.land/std@0.174.0/http/server.ts";
 import { get_report } from "./audit.ts";
 import { get_doughtnut } from "./pie.ts";
 
-await serve(async (req) => {
-  const test = new URL(req.url).pathname.slice(1);
-
-  console.log(test);
-
-  if (!test || test === "") throw new Error("Missing test ID");
-
-  const { testUrl, breakdown_values, per_domain, first_party } =
-    await get_report(test);
-
-  const body = `
-<!DOCTYPE html>
+const index = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Component audit for ${testUrl}</title>
+    <title>Guardian Components audit</title>
+</head>
+<body>
+    <h1>Missing test id</h1>
+    <p>
+        To get a test report, please provide a WebPageTest id, e.g. : <a href="/230117_BiDcKE_8T2">/230117_BiDcKE_8T2</a>
+    </p>
+</body>
+</html>
+`;
+
+const html = (body: string) =>
+  new Response(body, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+
+await serve(async (req) => {
+  const test = new URL(req.url).pathname.slice(1);
+
+  console.log(test);
+
+  if (!test || test === "") return html(index);
+
+  const { testUrl, breakdown_values, per_domain, first_party } =
+    await get_report(test);
+
+  const body = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guardian Components audit – ${testUrl}</title>
 </head>
 <body>
     <h1>Component audit for ${testUrl}</h1>
@@ -30,9 +53,5 @@ await serve(async (req) => {
 </html>
 `;
 
-  return new Response(body, {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
+  return html(body);
 });
