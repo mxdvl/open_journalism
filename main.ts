085eaa309a6fe72fb00367b80e2711cd7016e139
Max Duval <max.duval@theguardian.com>
2023-04-06 17:03:31 +0100

    fix: cleanup unused modules

--- main.ts
+++ main.ts
@@ -1,28 +1,4 @@
 import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
-import { get_report } from "./audit.ts";
-import { get_map } from "./cached.ts";
-import { get_table } from "./table.ts";
-
-const template = await Deno.readTextFile("./index.html");
-
-const to_string = (array: Record<string, string | number>[]) =>
-  array.map((item) =>
-    "{" + Object.entries(item).map(([key, value]) =>
-      `${key}: ${typeof value === "number" ? value : `"${value}"`}`
-    ) + "}"
-  );
-
-const html = (title: string, body: string) =>
-  new Response(
-    template
-      .replace("<!-- title -->", title)
-      .replace("<!-- body -->", body),
-    {
-      headers: {
-        "Content-Type": "text/html",
-      },
-    },
-  );
 
 await serve(async (req) => {
   try {
